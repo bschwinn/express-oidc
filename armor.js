cab1a35181c42efbef3637bb8ca58e961cb4c7c3
bschwinn <bschwinn7@gmail.com>
2020-10-19 23:25:04 -0400

    add passport

--- armor.js
+++ armor.js
@@ -1,17 +1,11 @@
-const superSweetPasswords = ["bluenoyellow", "12345", "noneshallpass", "prettyplease"];
-
-export const checkPassword = (pwd) => {
-    return superSweetPasswords.includes(pwd);
-}
 // super awesome authentication middleware
 export const authenticator = (req, res, next) => {
-    const cook = req.cookies.session;
-    if (checkPassword(cook)) {
+    if (req.user) {
         return next()
+    } else {
+        return res
+            .set('Content-Type', 'application/json')
+            .status(401)
+            .json({ message: 'Unauthenticated' })
     }
-    console.log(`not authorized, path: ${req.path}`)
-    if (req.path.startsWith('/api')) {
-        return res.status(401).json({ "status" : "unauthorized" });
-    }
-    return res.redirect('/login');
 }
--- server.js
+++ server.js
@@ -3,8 +3,12 @@ import path from 'path'
 import express from 'express'
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv'
+import expressSession from 'express-session'
+import passport from 'passport'
+import openIdClient from 'openid-client'
+import redisStore from 'connect-redis'
 import storage from './storage.js'
-import {authenticator, checkPassword} from './armor.js'
+import {authenticator} from './armor.js'
 
 // app init
 dotenv.config()
@@ -20,14 +24,63 @@ app.get('/login', (req, res) => {
     console.log(`sending file: ${filePath}`);
     return res.sendFile(filePath);
 });
-app.post('/login', (req, res) => {
-    if (checkPassword(req.body.secret)) {
-        console.log('setting up session');
-        res.cookie('session', req.body.secret);
-        return res.redirect(`/secret`);
-    }
-    return res.redirect(`/login`);
+
+const { Issuer, Strategy, custom } = openIdClient
+custom.setHttpOptionsDefaults({
+  timeout: 10000,
 });
+
+const openIDCIssuer = await Issuer.discover(process.env.OIDC_OAUTH_URL)
+const redirectUri = process.env.OIDC_REDIRECT_URI
+const client = new openIDCIssuer.Client({
+  client_id: process.env.OIDC_CLIENT_ID,
+  client_secret: process.env.OIDC_CLIENT_SECRET,
+  redirect_uris: [`${process.env.HOST_URL}${redirectUri}`],
+});
+
+const RedisStore = redisStore(expressSession);
+const seshStore = new RedisStore({ client: stats.client });
+
+app.use(expressSession({
+    cookie: {
+      maxAge: 86400000,
+      httpOnly: true,
+      secure: false,
+    },
+    saveUninitialized: false,
+    resave: false,
+    secret: process.env.EXPRESS_SESSION_SECRET,
+    store: seshStore,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+    'oidc',
+    new Strategy(
+      { client, passReqToCallback: true },
+      async (req, tokenSet, done) => {
+        const user = tokenSet.claims()
+        done(null, user)
+      }
+    )
+)
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
+
+app.get('/oauth', (req, res, next) => {
+    console.log('doing the oidc thing');
+    passport.authenticate('oidc', { logout: 'true' })(req, res, next);
+});
+
+app.get(redirectUri, (req, res, next) =>
+    passport.authenticate('oidc', {
+        successRedirect: '/secret/',
+        failureRedirect: '/login',
+    })(req, res, next)
+)
 
 // public tracking API
 app.put('/track', (req, res) => {
