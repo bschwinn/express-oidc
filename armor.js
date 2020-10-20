c9a540c6975678686b64bf525c82f77fedb2161b
bschwinn <bschwinn7@gmail.com>
2020-10-19 23:53:56 -0400

    less changes

--- armor.js
+++ armor.js
@@ -6,5 +6,5 @@ export const authenticator = (req, res, next) => {
     if (req.path.startsWith('/api')) {
         return res.status(401).json({ "status" : "unauthorized" });
     }
-    res.redirect('/login');
+    return res.redirect('/login');
 }
